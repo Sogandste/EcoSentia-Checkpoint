8b995b46379cb1f3aa996f28902fd2207c31fe9b
Sogandste <122667543+Sogandste@users.noreply.github.com>
2026-08-21 19:52:40 +0330

    Update app.py

--- app.py
+++ app.py
@@ -273,5 +273,5 @@ def server_error(_):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "7860"))
+    port = int(os.environ.get("PORT", "10000"))
     app.run(host="0.0.0.0", port=port, debug=False)
